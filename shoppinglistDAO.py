a0120aa25fb8cb95f272da4b774cef81873cde7b
Quinn <Keith.Quinn@bsci.com>
2021-12-13 17:20:49 +0000

    Adding code to DAO to resolve DB timeout issues

--- shoppinglistDAO.py
+++ shoppinglistDAO.py
@@ -4,72 +4,60 @@ import dbconfig as cfg
 class shoppinglistDAO:
     db=""
 
-    def initConnectToDB(self):
-        db = mysql.connector.connect(
+    def dbConnect(self):
+            self.db = mysql.connector.connect(
             host=cfg.mysql['host'],
             user=cfg.mysql['user'],
             password=cfg.mysql['password'],
             database=cfg.mysql['database'],
-            pool_name='my_connection_pool',
-            pool_size=10 
-        )
-        return db
-
-    def getConnection(self):
-        db = mysql.connector.connect(
-            pool_name='my_connection_pool'
-        )
-        return db
+            )
 
     def __init__(self):
-        db = self.initConnectToDB()
-        db.close()
+        self.dbConnect()
+
+
+    def getCursor(self):
+        if not self.db.is_connected():
+            self.dbConnect()
+        return self.db.cursor()
     
     def create(self, values):
-        db = self.getConnection()
-        cursor = db.cursor()
+        cursor = self.getCursor()
         sql="insert into shoppinglist (item, brand, quantity) values (%s,%s,%s)"
         cursor.execute(sql, values)  
         self.db.commit()
-        lastRowId = cursor.lastrowid
-        db.close()
-        return lastRowId
+        cursor.close()
+        return cursor.lastrowid
 
     def getAll(self):
-        db = self.getConnection()
-        cursor = db.cursor()
+        cursor = self.getCursor()
         sql="select * from shoppinglist"
         cursor.execute(sql)
         results = cursor.fetchall()
         returnArray = []
         for result in results:
             returnArray.append(self.convertToDictionary(result))
-        db.close()
+        cursor.close()
         return returnArray
 
     def findByID(self, id):
-        db = self.getConnection()
-        cursor = db.cursor()
+        cursor = self.getCursor()
         sql="select * from shoppinglist where id = %s"
         values = (id,)
         cursor.execute(sql, values)
         result = cursor.fetchone()
         cursor.close()
-        shop = self.convertToDictionary(result)
-        db.close()
-        return shop
+        return self.convertToDictionary(result)
 
     def update(self, values):
-        db = self.getConnection()
-        cursor = db.cursor()
+        cursor = self.getCursor()
         sql="update shoppinglist set item= %s, brand=%s, quantity=%s where id = %s"
         cursor.execute(sql, values)
         self.db.commit()
-        db.close()
+        cursor.close()
     
     def delete(self, id):
-        db = self.getConnection()
-        cursor = db.cursor()
+        cursor = self.getCursor()
         sql="delete from shoppinglist where id = %s"
         values = (id,)
         cursor.execute(sql, values)
